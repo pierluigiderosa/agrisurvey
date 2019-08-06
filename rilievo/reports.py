23559ef4e2a73d76882c6655a4764e4574f09240
pierluigiderosa <pierluigi.derosa@gmail.com>
2019-08-06 15:56:32 +0200

    first final version of report complete

--- rilievo/reports.py
+++ rilievo/reports.py
@@ -30,7 +30,8 @@ def report_singolo(response,id_danno):
 def add_page_number(canvas, doc):
     canvas.saveState()
     canvas.setFont('Times-Roman', 10)
-    page_number_text = "%d" % (doc.page)
+    canvas.setTitle('Report Agrisurvey')
+    page_number_text = "pagina %d" % (doc.page)
     canvas.drawCentredString(
         0.75 * inch,
         0.75 * inch,
@@ -40,6 +41,12 @@ def add_page_number(canvas, doc):
 
 def report_singolo_platypus(id,nome,cognome,agricoltore,danno,anagrafica,rilievi):
     tf=tempfile.NamedTemporaryFile()
+
+    stile_tabella=TableStyle([
+        ('TEXTCOLOR', (0, 0), (0, -1), colors.blue),
+        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+    ])
 
     doc = SimpleDocTemplate(tf.name,
                             pagesize=A4,
@@ -66,64 +73,109 @@ def report_singolo_platypus(id,nome,cognome,agricoltore,danno,anagrafica,rilievi
     Story.append(t)
     Story.append(Spacer(2 * cm, 2 * cm))
     Story.append(Paragraph(
-        "Dati dell'agricoltore che ha fatto domanda",
+        "Dati inseriti nel portale in fase di domanda",
         styles['Heading1']))
+    Story.append(HRFlowable(width=20*cm, thickness=0.2, color=colors.darkgrey))
+    Story.append(Paragraph(
+        "Dati dell'agricoltore che ha fatto domanda",
+        styles['Heading2']))
     dettagli_agricoltore = [['Luogo di nascita', str(agricoltore.luogoNascita)],
                             ['data di nascita',agricoltore.dataNascita],
+                            ['Codice Fiscale', agricoltore.CF],
                             ['pec',agricoltore.pec],
                             ['telefono',agricoltore.telefono],
+                            ['Referente aziendale',agricoltore.Referente],
+                            ['Telf.referente aziendale', agricoltore.RefTel],
                             ['Residenza',agricoltore.viaResidenza],
                             ['comune di residenza',agricoltore.ComuneRes],
                             ['P. Iva',agricoltore.PIva],
                             ['nome azienda',agricoltore.azNome],
                             ['località azienda',agricoltore.azLoc],
+                            ['Azienda biologica',agricoltore.biologica],
+                            ['Iban',agricoltore.iban],
                             ['Comune azienda',agricoltore.azComune]
                             ]
     t = Table(dettagli_agricoltore, 2 * [5.5 * cm], len(dettagli_agricoltore)  * [1 * cm], hAlign='LEFT')
+    t.setStyle(stile_tabella)
     Story.append(t)
     Story.append(Spacer(2 * cm, 2 * cm))
     Story.append(Paragraph(
-        "Dati inseriti nella domanda",
-        styles['Heading1']))
-    danno_inserito = [['Coltura',danno.coltura],
+        "Dati inseriti nella domanda di danno",
+        styles['Heading2']))
+    danno_inserito = [['Foglio catastale',danno.foglio],
+                      ['Particella catastale', danno.particella],
+                      ['Superficie totale particelle ettari',danno.SumTot],
+                      ['Superficie seminativa particella ettari', danno.SumSem],
+                      ['% stimata del danno', danno.PercDanno],
+                      ['Produzione prevista nella particella',danno.Produzione],
+                      ['% produzione persa',danno.PerProdPersa],
+                      ['ipotetico valore del danno stimato dal dichiarante',danno.ValoreDanno],
+                      ['numero piante danneggiate',danno.NumPianteDan],
+                      ['Selvaggina che ha causato i danni alla coltura',danno.SelvagginaSem],
+                      ['Opere di protezione presenti',danno.OpereProtezione],
+                      ['Coltura',danno.coltura],
                       ['varietà',danno.varieta],
                       ['Tipologia di piante',danno.TipoPiante],
                       ['Polizza assicurativa',danno.polizza],
                       ['Azienda biologica',danno.biologica],
                       ['iban',danno.iban],
-                      ['data inserimento domanda',str(danno.data_ins.day)+'/'+str(danno.data_ins.month)+'/'+str(danno.data_ins.year)]
+                      ['data inserimento domanda',str(danno.data_ins.day)+'/'+str(danno.data_ins.month)+'/'+str(danno.data_ins.year)],
+                      ['Note',danno.note]
                       ]
-    t = Table(danno_inserito, 2 * [5.5 * cm], len(danno_inserito) * [1 * cm], hAlign='LEFT')
+    if danno.data_danno is not None:
+        danno_inserito.append(['Data danno presunta',
+         str(danno.data_danno.day) + '/' + str(danno.data_danno.month) + '/' + str(danno.data_danno.year)])
+
+    t = Table(danno_inserito, 2 * [8.5 * cm], len(danno_inserito) * [1 * cm], hAlign='LEFT')
+    t.setStyle(stile_tabella)
     Story.append(t)
     Story.append(PageBreak())
 
     Story.append(Paragraph(
         "Rilievo svolto da %s %s" %(danno.Rilevatore.first_name,danno.Rilevatore.last_name),
         styles['Heading1']))
+    Story.append(HRFlowable(width=20 * cm, thickness=0.2, color=colors.darkgrey))
     Story.append(Paragraph(
         "Anagrafica",
         styles['Heading2']))
-    #prendo la firma
+    #prendo la firma agricoltore
     img = utils.ImageReader(anagrafica.Firma.path)
     width = 5 * cm
     iw, ih = img.getSize()
     aspect = ih / float(iw)
     Firma = Image(anagrafica.Firma.path, width=width, height=(width * aspect))
+
+    #prendo firma rilevatore
+    img_ril = utils.ImageReader(anagrafica.Firma_rilevatore.path)
+    width = 5 * cm
+    iw, ih = img_ril.getSize()
+    aspect = ih / float(iw)
+    Firma_ril = Image(anagrafica.Firma_rilevatore.path, width=width, height=(width * aspect))
+
     anagrafica_tabella = [['mappale',anagrafica.mappa],
                           ['Rappresentante aziendale',anagrafica.Rapp_Az],
                           ['Documento rappresentante',anagrafica.Rapp_Az_documento],
                           ['Denominazione azienda',anagrafica.Az_Ag_denominazione],
                           ['Titolo di possesso',anagrafica.Az_Ag_possesso],
-                          ['indirizzo',anagrafica.Az_Ag_indirizzo]
+                          ['indirizzo',anagrafica.Az_Ag_indirizzo],
+                          ['data di sopralluogo',str(anagrafica.data_sopralluogo.day)+'/'+str(anagrafica.data_sopralluogo.month)+'/'+str(anagrafica.data_sopralluogo.year)],
+                          ['Tecnico incaricato',anagrafica.tecnico_incaricato]
                           ]
     t = Table(anagrafica_tabella, 2 * [5.5 * cm], len(anagrafica_tabella) * [1 * cm], hAlign='LEFT')
+    t.setStyle(stile_tabella)
     Story.append(t)
     Story.append(Paragraph(
-        "Firma",
+        "Firma Agricoltore/Rappresentante aziendale",
         styles['Normal']))
     Firma.hAlign = 'CENTER'
     Story.append(Firma)
-    Story.append(Spacer(2 * cm, 2 * cm))
+    Story.append(Spacer(0.5 * cm, 0.5 * cm))
+    Story.append(Paragraph(
+        "Firma Tecnico incaricato",
+        styles['Normal']))
+    Firma_ril.hAlign = 'CENTER'
+    Story.append(Spacer(0.5 * cm, 0.5 * cm))
+    Story.append(Firma_ril)
 
     Story.append(Paragraph(
         "Elenco dei rilievi singoli",
@@ -131,25 +183,42 @@ def report_singolo_platypus(id,nome,cognome,agricoltore,danno,anagrafica,rilievi
     count=1
     for rilievo in rilievi:
         rilievo_data=[
-                      ['Opere di prevenzione',rilievo.OperePrevenzione],
-                      ['Comune',rilievo.comune],
-                      ['coltura',rilievo.coltura],
-                      ['Localizzazione fondo',rilievo.Localizzazione_fondo],
-                      ['produzione media',rilievo.prod_media],
-                      ['Superficie totale',rilievo.Sup_totale],
-                      ['Prima specie',rilievo.Specie1],
-                      ['Seconda specie',rilievo.Specie2],
-                      ['Superficie danneggiata',rilievo.Sup_danneggiata],
-                      ['Percentuale danno',rilievo.perc_danno],
-                      ['particella', rilievo.particelle],
-                      ['foglio',rilievo.foglio],
-                    ['varietà',rilievo.varieta]
-                      ]
-        t = Table(rilievo_data, 2 * [5.5 * cm], len(rilievo_data) * [1 * cm], hAlign='LEFT')
+            ['Opere di prevenzione',rilievo.OperePrevenzione],
+            ['Altre opere di prevenzione',rilievo.OperePrevenzione_altro],
+            ['Comune',rilievo.comune],
+            ['coltura',rilievo.coltura],
+            ['Altra coltura',rilievo.coltura_altro],
+            ['Localizzazione fondo',rilievo.Localizzazione_fondo],
+            ['Altra localizzazione del fondo',rilievo.Localizzazione_fondo_altro],
+            ['produzione media',rilievo.prod_media],
+            ['Superficie totale',rilievo.Sup_totale],
+            ['Prima specie',rilievo.Specie1],
+            ['Prima specie altro',rilievo.specie1_altro],
+            ['% danno specie 1',rilievo.perc_specie1],
+            ['Seconda specie',rilievo.Specie2],
+            ['Seconda specie altro',rilievo.Specie2_altro],
+            ['% danno specie 2',rilievo.perc_specie2],
+            ['Superficie danneggiata',rilievo.Sup_danneggiata],
+            ['Percentuale danno',rilievo.perc_danno],
+            ['particella', rilievo.particelle],
+            ['foglio',rilievo.foglio],
+            ['varietà',rilievo.varieta],
+            ['Stato vegetazionele e fitosanitario',rilievo.statovegsanitario],
+            ['Numero di piante da sostituire',rilievo.numpiantesostituire],
+            ['Funzionalita Prevenzione',rilievo.FunzionalitaPrevenzione],
+            ['Coltura biologica',rilievo.colturabiologica],
+            ['superficie di risemina',rilievo.superficierisemina],
+            ['quantità di prodotto perduto',rilievo.quantitaprodotto],
+
+            ['note: ',rilievo.note_colturali]
+        ]
+        t = Table(rilievo_data, 2 * [7.0 * cm], len(rilievo_data) * [1 * cm], hAlign='LEFT')
+        t.setStyle(stile_tabella)
         Story.append(Paragraph(
             "Rilievo %s" % count,
             styles['Heading4']))
         Story.append(HRFlowable(width=20*cm, thickness=0.2, color=colors.grey))
+        Story.append(Spacer(1 * cm, 1 * cm))
 
         Story.append(t)
         Story.append(Spacer(2 * cm, 2 * cm))
